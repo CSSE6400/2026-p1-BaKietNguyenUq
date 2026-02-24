79c8031ef570e83c992c1ec22b3fea64033a261a
Ba Kiet Nguyen Uq <bakiet.nguyen@student.uq.edu.au>
2026-02-24 15:11:35 +1000

    Fix __init__.py

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from todo.views.routes import api  # Import the blueprint
+
 def create_app():
     app = Flask(__name__)
     
-    from .views.routes import api
+    # Register the blueprint
     app.register_blueprint(api)
+    
     return app
